8924931ab04979c7a8cd4ddb423d039926a5c829
leevely <yooo0220@nate.com>
2017-02-25 14:54:20 +0900

    README.md 수정

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 'use strict';
 $(document).ready(function () {
 
-    $(".side_menu_btn").on("click", function () {
-       $(".side_menu").stop().animate({
-          left: 0
-       }, 500);
-        $(".side_shadow").stop().fadeIn(300);
+    var sideMenu = new SideMenu({
+        selector: ".side_menu",
+        showBtnSelector: ".side_menu_btn",
+        shadowSelector: "side_shadow"
     });
-
-    $(".side_menu .close_btn").on("click", function () {
-        $(".side_menu").stop().animate({
-            left: -500
-        }, 500);
-        $(".side_shadow").stop().fadeOut(300);
-    });
-
-    $(".side_menu .main_menu > li > a").on("click", function () {
-        $(".sub_menu").not($(this).siblings(".sub_menu")).stop(false, true).slideUp(500);
-        $(this).siblings(".sub_menu").stop(false, true).slideDown(500);
-    });
-
 
 });
